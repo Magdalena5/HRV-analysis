06e536f6e92e1e68c042b7eafebf7df32bff13eb
Magdalena5 <36775766+Magdalena5@users.noreply.github.com>
2019-11-09 20:44:47 +0100

    Update HRV.m

--- HRV.m
+++ HRV.m
@@ -1,19 +1,34 @@
 
 %load data
-load('here_path_to_your_data');
-sig=X6;
+sig=load('here_path_to_your_data');
 signal_NN=sig(:);
 l=length(sig);
 
-[AVNN,SDNN,RMSSD,pNN50] = timedomain (signal_NN)
+
+
 signal_NN=signal_NN./1000;
-t=cumsum(signal_NN)./60;
-[LFnu,HFnu,LFHFfft] = frequencydomain (signal_NN,t)
+t=cumsum(signal_NN);
+% Remove ectopic beats
+signal_NN(signal_NN < 0.2 | signal_NN > 2000) = NaN;
+for i= 2:l
+    signal_NN(abs(signal_NN(i-1)-signal_NN(i)) <= 0.2 * signal_NN(i-1)) = NaN;
+end
+t2 =t(1):t(end);
+
+%signal interpolation
+signal_NNt = interp1(t,signal_NN,t2,'spline');
+signal_NNt=signal_NNt.*1000;
+[AVNN,SDNN,RMSSD,pNN50] = timedomain (signal_NNt)
+
+%interpolcja sygnału
+ t2 =t(1):0.2:t(end);
+ signal_NNf = interp1(t,signal_NN,t2,'spline');
+[LFnu,HFnu,LFHFfft] = frequencydomain (signal_NNf)
 
 [ sd1, sd2,sd1sd2,x_old,y_old,sd1_line_old,sd2_line_old] = poincareplot(signal_NN);
 
 figure(1)
-plot(t,signal_NN);
+plot(t,sig);
 title('tachogram');xlabel('Time');ylabel('RR(s)');
 
 %Poincare plot
